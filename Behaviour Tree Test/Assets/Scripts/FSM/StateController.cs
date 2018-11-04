67faf7a28bb1920acd516c25bb7c93f1cf624c0b
Felipe Pessoa <felipessoaf@hotmail.com>
2018-11-03 22:24:56 -0300

    FSM lib

---
+++ Behaviour Tree Test/Assets/Scripts/FSM/StateController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class StateController : MonoBehaviour
+{
+    public State currentState;
+    public State remainState;
+    public bool AIActive;
+
+    [HideInInspector] public List<Transform> wayPointList;
+    [HideInInspector] public int nextWayPoint;
+    [HideInInspector] public Transform chaseTarget;
+    [HideInInspector] public float stateTimeElapsed;
+
+    void Update()
+    {
+        if (!AIActive)
+            return;
+        currentState.UpdateState(this);
+    }
+
+    public void TransitionToState(State nextState)
+    {
+        if (nextState != remainState)
+        {
+            currentState = nextState;
+            OnExitState();
+        }
+    }
+
+    public bool CheckIfCountDownElapsed(float duration)
+    {
+        stateTimeElapsed += Time.deltaTime;
+        return (stateTimeElapsed >= duration);
+    }
+
+    private void OnExitState()
+    {
+        stateTimeElapsed = 0;
+    }
+}
