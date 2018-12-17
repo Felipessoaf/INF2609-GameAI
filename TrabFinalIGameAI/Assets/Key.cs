05e8088a84b8ba24474faa2a37134866054fa9bf
Felipe Pessoa <felipessoaf@hotmail.com>
2018-12-17 20:47:47 -0200

    key random spawn point

---
+++ TrabFinalIGameAI/Assets/Key.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour {
+
+    public List<Transform> SpawnPoints;
+
+    private void Awake()
+    {
+        Vector3 pos = SpawnPoints[Random.Range(0, SpawnPoints.Count)].position;
+        pos.y = 1.8f;
+        transform.position = pos;
+    }
+}
